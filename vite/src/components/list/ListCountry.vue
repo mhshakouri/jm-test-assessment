0838aa98187e3d1a59dc747da9ecd161aa7b70be
Hossein Shakouri <mhshakouri@gmail.com>
2025-11-15 08:56:24 +0330

    style(ui): added some styles to cards and list and color toggler

--- vite/src/components/list/ListCountry.vue
+++ vite/src/components/list/ListCountry.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
         <template v-if="countries.length">
             <ListCountryCard v-for="(country, index) in countries" :key="`${country.name}-${country.alpha3Code}`" :country="country" :preload="index <= 3" />
         </template>
--- vite/src/components/list/ListCountryCard.vue
+++ vite/src/components/list/ListCountryCard.vue
@@ -1,7 +1,7 @@
 <template>
-    <button>
+    <button class="flex flex-col gap-2 cursor-pointer bg-jm-white text-jm-blue-darker dark:bg-jm-blue-lighter dark:text-jm-gray-lighter py-3 px-2 rounded-md focus:outline-none focus:ring-2 focus:ring-jm-blue-lighter dark:focus:ring-jm-gray-lighter hover:bg-jm-gray-lighter dark:hover:bg-jm-blue-darker transition-colors duration-200 w-full md:hover:shadow-md">
         <Flag v-if="country.flags.png?.length" :flag="country.flags" :name="country.name" :preload="preload" />
-        <span v-if="country.name?.length" class="font-extrabold text-normal">{{ country.name }}</span>
+        <span v-if="country.name?.length" class="font-extrabold text-normal self-start">{{ country.name }}</span>
         <span>
             <UiKeyValue v-if="country.population !== undefined" title="Population" :description="(country.population === 'N/A' || country.population === undefined || country.population === 0) ? 'N/A' : country.population.toLocaleString('en-US')" />
             <UiKeyValue v-if="country.region" title="Region" :description="country.region" />
