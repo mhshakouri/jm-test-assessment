40f781cf0132a9e500aff7559461a3704e564a9a
Hossein Shakouri <mhshakouri@gmail.com>
2025-11-15 09:04:40 +0330

    style(code): formatted code, and separated sort function

--- vite/src/composables/useCountries.ts
+++ vite/src/composables/useCountries.ts
@@ -9,9 +9,12 @@ import { useSort } from "./useSort";
 const useCountriesComposable = () => {
   const { searchText, searchAll, isSearching } = useSearch();
   const { regionFilter } = useRegionFilter();
-  const defaultSort = ref<SortOptions>('name');
-  const defaultSortOrder = ref<SortOrders>('asc');
-  const { sort, sortOrder } = useSort(defaultSort.value, defaultSortOrder.value);
+  const defaultSort = ref<SortOptions>("name");
+  const defaultSortOrder = ref<SortOrders>("asc");
+  const { sort, sortOrder } = useSort(
+    defaultSort.value,
+    defaultSortOrder.value
+  );
   const countriesRepository = ref<CountriesRepository>(new Map());
 
   const region = computed(() =>
@@ -20,12 +23,13 @@ const useCountriesComposable = () => {
       : undefined
   );
   const countriesArray = computed<Country[]>(() =>
-    Array.from(countriesRepository.value.values()).sort((a, b) => (sort.value === defaultSort.value ? a.name.localeCompare(b.name) : a.population === 'N/A' ? 0 : b.population === 'N/A' ? 0 : a.population - b.population) * (sortOrder.value === defaultSortOrder.value ? 1 : -1))
+    sortedCountries(Array.from(countriesRepository.value.values()))
   );
   const countries = computed<Country[]>(() =>
     region.value && !searchAll.value
       ? countriesArray.value.filter(
-          (country) => country.region.toLowerCase() === region.value?.toLowerCase()
+          (country) =>
+            country.region.toLowerCase() === region.value?.toLowerCase()
         )
       : countriesArray.value
   );
@@ -36,30 +40,34 @@ const useCountriesComposable = () => {
   );
   const countriesList = computed<Country[]>(() => {
     if (!isSearching.value) {
-        return countries.value;
+      return countries.value;
     }
     return countriesBySearch.value;
   });
 
   const setCountry = (data: Partial<Country>) => {
     if (!data.alpha3Code) return;
-    
+
     const currentCountry = countriesRepository.value.get(data.alpha3Code);
     if (!currentCountry) {
+      let population = data.population ?? "N/A";
+      population = !isNaN(Number(population)) ? Number(population) : "N/A";
       countriesRepository.value.set(data.alpha3Code, {
-        ...data as Country,
-        population: data.population ?? 'N/A',
-        capital: data.capital ?? 'N/A',
+        ...(data as Country),
+        population: population,
+        capital: data.capital ?? "N/A",
       });
     } else {
+      let population = data.population ?? currentCountry.population;
+      population = !isNaN(Number(population)) ? Number(population) : "N/A";
       countriesRepository.value.set(data.alpha3Code, {
         alpha3Code: data.alpha3Code ?? currentCountry.alpha3Code,
         region: data.region ?? currentCountry.region,
         name: data.name ?? currentCountry.name,
         nativeName: data.nativeName ?? currentCountry.nativeName,
-        population: data.population ?? currentCountry.population ?? 'N/A',
+        population: population,
         subregion: data.subregion ?? currentCountry.subregion,
-        capital: data.capital ?? currentCountry.capital ?? 'N/A',
+        capital: data.capital ?? currentCountry.capital ?? "N/A",
         flags: data.flags ?? currentCountry.flags,
         topLevelDomain: data.topLevelDomain ?? currentCountry.topLevelDomain,
         currencies: data.currencies ?? currentCountry.currencies,
@@ -71,8 +79,27 @@ const useCountriesComposable = () => {
   const setCountries = (data: Partial<Country>[]) => {
     (data ?? []).forEach((country) => setCountry(country));
   };
+  const sortedCountries = (data: Country[]) => {
+    return data.sort(
+      (a, b) =>
+        (sort.value === defaultSort.value
+          ? a.name.localeCompare(b.name)
+          : a.population === "N/A"
+          ? 0
+          : b.population === "N/A"
+          ? 0
+          : a.population - b.population) *
+        (sortOrder.value === defaultSortOrder.value ? 1 : -1)
+    );
+  };
 
-  return { setCountries, setCountry, countriesList, countriesRepository, countriesArray };
+  return {
+    setCountries,
+    setCountry,
+    countriesList,
+    countriesRepository,
+    countriesArray,
+  };
 };
 
 export const useCountries = createSharedComposable(useCountriesComposable);
