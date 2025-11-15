e128fa3594ccab3c54024bdd8e2140a545f56c87
Hossein Shakouri <mhshakouri@gmail.com>
2025-11-15 14:43:22 +0330

    style(ui): added some styles to desktop version of the country details

--- vite/src/components/country/CountryDetail.vue
+++ vite/src/components/country/CountryDetail.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="flex flex-col gap-12">
+  <div class="flex flex-col gap-12 md:flex-row md:justify-between md:items-start md:gap-6">
     <Flag
       v-if="country.flags?.png"
       :flag="country.flags"
       :name="country.name"
       :preload="true"
+      class="md:w-2/5 md:max-w-2/5 md:shrink-0"
     />
-    <div class="flex flex-col gap-6">
+    <div class="flex flex-col gap-6 md:py-4 md:w-3/5 md:max-w-3/5 md:min-w-0">
       <h1 class="self-start">
         <UiKeyValue v-if="country.name" :title="country.name" />
       </h1>
-      <div class="flex flex-col gap-2">
-        <div class="flex flex-col gap-2">
+      <div class="flex flex-col gap-2 md:flex-row md:min-w-0">
+        <div class="flex flex-col gap-2 md:min-w-0">
           <UiKeyValue
             v-if="country.nativeName"
             title="Native Name:"
@@ -31,6 +32,7 @@
             v-if="country.subregion"
             title="Subregion:"
             :description="country.subregion"
+            class="md:flex-col"
           />
           <UiKeyValue
             v-if="country.capital && country.capital !== 'N/A'"
@@ -38,7 +40,7 @@
             :description="country.capital"
           />
         </div>
-        <div class="flex flex-col gap-2">
+        <div class="flex flex-col gap-2 md:min-w-0 md:flex-1">
           <UiKeyValue
             v-if="country.topLevelDomain?.length"
             title="Top Level Domain:"
@@ -56,7 +58,7 @@
               </div>
             </template>
           </UiKeyValue>
-          <div class="flex flex-col gap-2 w-full overflow-hidden">
+          <div class="flex flex-col gap-2 w-full overflow-hidden min-w-0">
             <UiKeyValue
               v-if="country.languages?.length"
               title="Languages:"
@@ -65,7 +67,7 @@
             >
               <template #default>
                 <div
-                  class="flex flex-row gap-2 overflow-x-auto pb-2 -mb-2 w-full"
+                  class="flex flex-row gap-2 overflow-x-auto pb-2 -mb-2 w-full min-w-0"
                 >
                   <span
                     v-for="language in country.languages"
@@ -80,7 +82,7 @@
           </div>
         </div>
       </div>
-      <div class="flex flex-col gap-2 w-full overflow-hidden">
+      <div class="flex flex-col gap-2 w-full overflow-hidden min-w-0">
         <UiKeyValue
           v-if="borderCountries.length"
           title="Border Countries:"
@@ -88,7 +90,7 @@
           :full-width="true"
         >
           <template #default>
-            <div class="flex flex-row gap-2 overflow-x-auto pb-2 -mb-2 w-full">
+            <div class="flex flex-row gap-2 overflow-x-auto pb-2 -mb-2 w-full min-w-0">
               <CountryButton
                 v-for="borderCountry in borderCountries"
                 :key="borderCountry.alpha3Code"
